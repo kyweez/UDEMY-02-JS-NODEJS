228904c6627ab2b56000e55b50b7d7687c1f248b
kyweez <auboudier.pro@gmail.com>
2020-05-25 17:56:37 +0200

    Refactoring folders and files

---
+++ 00-TRAINING-JS/01-VARIABLES-AND-OPERATORS/00-variable-declaration.js
@@ -0,0 +1,16 @@
+// String
+let name = "Marc";
+name = 'John';
+console.log(name);
+
+// Number
+let age = 21;
+console.log(age);
+
+// Boolean
+let majoriry = true;
+console.log(majoriry);
+
+// Constant
+const name2 = "Marie";
+console.log(name2);
