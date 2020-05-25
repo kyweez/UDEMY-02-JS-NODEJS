417dfe85fa5118a3e358c370e7a148a3acd3b66c
kyweez <auboudier.pro@gmail.com>
2020-05-25 20:58:00 +0200

    User interaction OK

---
+++ 00-TRAINING-JS/02-USER-INTERACTION/main.js
@@ -0,0 +1,14 @@
+
+// Displays an information
+alert(`Hello World`);
+
+// Asks a confirm. Yes returns a true and no returns a false
+confirm(`Do you want to validate ?`)
+
+while (confirm(`Try this in a condition ?`)) {
+    alert(`It works !`);
+}
+
+// Asks the user an information 
+const age = prompt(`Insert your age`);
+alert(`You are ${age} years old`);
