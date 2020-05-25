6b5e743d661a2a456fdde3ad44797ebab8b20f12
kyweez <auboudier.pro@gmail.com>
2020-05-25 19:10:08 +0200

    Operators

---
+++ 00-TRAINING-JS/01-VARIABLES-AND-OPERATORS/02-math-operators.js
@@ -0,0 +1,14 @@
+// OPERATORS
+// - "+" addition
+// - "-" subtraction
+// - "*" multiplication
+// - "/" division
+// - "%" modulo <=> 5/2 = 2.45343 restant 1
+// - "**" exponentiation 5**2 = 25
+
+let number1 = 8, number2 = 4, result;
+result = (number1 + number2) * 9; // 108
+result++; // incrementation = add 1
+result--; // decrementation =  remove 1
+console.log(result);
+console.log(10**5)
