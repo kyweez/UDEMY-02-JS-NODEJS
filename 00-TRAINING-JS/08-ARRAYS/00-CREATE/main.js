397c299d9c1bd4e8166a78b1d9dac1ba63bd8d6b
kyweez <auboudier.pro@gmail.com>
2020-06-02 22:25:23 +0200

    #Arrays / Push before sleep

---
+++ 00-TRAINING-JS/08-ARRAYS/00-CREATE/main.js
@@ -0,0 +1,3 @@
+let myArray = new Array('one', 'two', 'three');
+let myArray = Array('one', 'two', 'three');
+let myArray = ['one', 'two', 'three'];
---
+++ 00-TRAINING-JS/08-ARRAYS/01-MULTIDIMENSIONAL-ARRAYS/main.js
@@ -0,0 +1,15 @@
+let myArray = new Array('one', 'two', 'three');
+let myArray = Array('one', 'two', 'three');
+let myArray = ['one', 'two', 'three'];
+
+// old version
+let array2D = new Array(
+    Array('Mark', 'Jeff', 'Bill'),
+    Array('Zuckerberg', 'Bezos', 'Gates')  
+);
+
+// new version
+let monTableau2D = [
+    ['Mark', 'Jeff', 'Bill'],
+    ['Zuckerberg', 'Bezos', 'Gates']
+];
