7dd6c6da4498379318f902c7da3d2c20cc7c9f3f
kyweez <auboudier.pro@gmail.com>
2020-08-16 23:14:29 +0200

    #Arrays / Concat

---
+++ 00-TRAINING-JS/08-ARRAYS/07-CONCAT/main.js
@@ -0,0 +1,8 @@
+let monTableau = ['un', 'deux', 'trois', 'quatre'];
+// console.log(monTableau.join(' / '));
+
+let monTableau2D = [
+    ['Mark', 'Jeff', 'Bill'],
+    ['Zuckerberg', 'Bezos', 'Gates']
+];
+console.log(monTableau2D.join(' / '));
