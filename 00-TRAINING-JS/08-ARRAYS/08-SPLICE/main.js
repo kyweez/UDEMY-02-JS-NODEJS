4007b5e77976c53012c6f35bf0e486e1c31e2fa3
kyweez <auboudier.pro@gmail.com>
2020-08-16 23:21:48 +0200

    #Arrays / Splice function

---
+++ 00-TRAINING-JS/08-ARRAYS/08-SPLICE/main.js
@@ -0,0 +1,17 @@
+let monTableau = ['un', 'deux', 'trois', 'quatre'];
+monTableau.splice(1, 0, 'random', 'pie');
+// console.log(monTableau);
+
+let monTableau2D = [
+    ['Mark', 'Jeff', 'Bill'],
+    ['Zuckerberg', 'Bezos', 'Gates']
+];
+monTableau2D.splice(2, 0, ['30', '45', '70']);
+console.log(monTableau2D);
+
+let monTableauAssociatif = {
+    'prenom' : 'Mark',
+    'nom'    : 'Zuckerberg',
+    'poste'  : 'PDG de Facebook'
+};
+// console.log(monTableauAssociatif);
