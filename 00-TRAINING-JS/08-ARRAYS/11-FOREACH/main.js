d746a80418e5b8e79de5f4323be5f084f90ade09
kyweez <auboudier.pro@gmail.com>
2020-08-17 22:52:44 +0200

    #Arrays / Foreach

---
+++ 00-TRAINING-JS/08-ARRAYS/11-FOREACH/main.js
@@ -0,0 +1,11 @@
+let listeDePays = ['France', 'Belgique', 'Japon', 'Maroc'];
+
+// for (const pays of listeDePays) {
+//     console.log(pays);
+// }
+
+// listeDePays.forEach(function(pays){
+//     console.log(pays);
+// });
+
+listeDePays.forEach(pays => console.log(pays));
