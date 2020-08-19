ca139fd7bf9eacc7e32f3a2c310499ecd0994324
kyweez <auboudier.pro@gmail.com>
2020-08-19 12:00:00 +0100

    Fonctions flechees
    Date:      Thu Aug 20 12:00:00 2020 +0100

---
+++ 00-TRAINING-JS/09-ARROW-FUNCTIONS/arrow-functions.js
@@ -0,0 +1,12 @@
+let maFonction = function(){
+    console.log('test');
+  }
+  
+  let maFonction = () => {
+    console.log('test');
+  }
+  
+  let maFonction = () => console.log('test');
+  
+  // Dans la pratique, on l'utilise surtout pour les petites fonctions anonymes
+  // Ces fonctions fléchées n'ont pas de this
