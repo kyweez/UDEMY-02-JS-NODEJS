e13865947164705c19135cfbb5ca1fae95b97fb2
kyweez <auboudier.pro@gmail.com>
2020-08-22 12:00:00 +0100

    Destructuration
    Date:      Sat Aug 22 12:00:00 2020 +0100

---
+++ 00-TRAINING-JS/10-OBJECTS/01-DESTRUCTURE/main.js
@@ -0,0 +1,13 @@
+let informations = ['superSayen', '25', 'homme'];
+
+// Sans décomposition
+// let pseudo = informations[0];
+// let age    = informations[1];
+// let sexe   = informations[2];
+
+// Avec décomposition
+let [pseudo, age, sexe] = informations;
+
+console.log(pseudo);
+console.log(age);
+console.log(sexe);
