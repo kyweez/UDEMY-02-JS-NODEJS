6d1bbb815a935f408c7eadd86d6fe4692f4fe4b1
kyweez <auboudier.pro@gmail.com>
2020-09-09 00:14:56 +0200

    Objet Map

---
+++ 00-TRAINING-JS/10-OBJECTS/03-MAP/main.js
@@ -0,0 +1,15 @@
+let utilisateurs = new Map();
+
+utilisateurs.set('Mark Zuckerberg', {
+    email: 'mark@facebook.com',
+    poste: 'PDG',
+});
+
+utilisateurs.set('Bill Gates', {
+    email: 'bill@gatesnotes.com',
+    poste: 'Sauver le monde',
+});
+
+// utilisateurs.delete('Bill Gates');
+
+console.log(utilisateurs);
